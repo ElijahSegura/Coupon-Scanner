711e5a695a3e163c2a91d0af671d603fd62036f9
ElijahSegura <elijahsegurar@gmail.com>
2016-08-15 22:29:28 -0600

    testing to see if built in android VCS functionality works

--- app/src/main/java/com/example/elijahsegura/coupon_scanner/MainActivity.java
+++ app/src/main/java/com/example/elijahsegura/coupon_scanner/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //This is just a test please work
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
